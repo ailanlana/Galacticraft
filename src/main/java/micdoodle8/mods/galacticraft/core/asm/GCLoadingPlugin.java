326e02a16ee004bb5684bfa458afaa9187ad2a78
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2024-09-01 16:18:28 +0200

    Delete ASM (#91)
    * delete broken ASM code that never did anything in the full pack and that crashes the game anyways when you enable it
    
    * spotless
    
    * update
    
    * Do not depend on itself
    
    ---------
    
    Co-authored-by: Martin Robertz <dream-master@gmx.net>
    Co-authored-by: Raven Szewczyk <git@eigenraven.me>

--- src/main/java/micdoodle8/mods/galacticraft/core/asm/GCLoadingPlugin.java
+++ src/main/java/micdoodle8/mods/galacticraft/core/asm/GCLoadingPlugin.java
@@ -1,6 +1,5 @@
 package micdoodle8.mods.galacticraft.core.asm;
 
-import java.io.File;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -13,29 +12,18 @@ import org.apache.logging.log4j.Logger;
 import com.gtnewhorizon.gtnhmixins.IEarlyMixinLoader;
 
 import cpw.mods.fml.relauncher.IFMLLoadingPlugin;
-import cpw.mods.fml.relauncher.IFMLLoadingPlugin.MCVersion;
-import cpw.mods.fml.relauncher.IFMLLoadingPlugin.Name;
-import cpw.mods.fml.relauncher.IFMLLoadingPlugin.SortingIndex;
-import cpw.mods.fml.relauncher.IFMLLoadingPlugin.TransformerExclusions;
 import micdoodle8.mods.galacticraft.core.Constants;
 import micdoodle8.mods.galacticraft.core.mixins.Mixins;
 
-/**
- * Holds the rest of GC's class patching code.
- */
-@TransformerExclusions("micdoodle8.mods.galacticraft.core.asm")
-@MCVersion("1.7.10")
-@Name(Constants.COREMOD_NAME_SIMPLE)
-@SortingIndex(1500)
+@IFMLLoadingPlugin.MCVersion("1.7.10")
+@IFMLLoadingPlugin.Name(Constants.COREMOD_NAME_SIMPLE)
 public class GCLoadingPlugin implements IFMLLoadingPlugin, IEarlyMixinLoader {
 
-    static final Logger LOGGER = LogManager.getLogger(Constants.COREMOD_NAME_SIMPLE);
-    static boolean dev;
-    static File debugOutputDir;
+    private static final Logger LOGGER = LogManager.getLogger(Constants.COREMOD_NAME_SIMPLE);
 
     @Override
     public String[] getASMTransformerClass() {
-        return new String[] { "micdoodle8.mods.galacticraft.core.asm.GCTransformer" };
+        return null;
     }
 
     @Override
@@ -49,12 +37,7 @@ public class GCLoadingPlugin implements IFMLLoadingPlugin, IEarlyMixinLoader {
     }
 
     @Override
-    public void injectData(Map<String, Object> data) {
-        dev = !(boolean) data.get("runtimeDeobfuscationEnabled");
-        debugOutputDir = new File((File) data.get("mcLocation"), ".asm");
-        // noinspection ResultOfMethodCallIgnored
-        debugOutputDir.mkdir();
-    }
+    public void injectData(Map<String, Object> data) {}
 
     @Override
     public String getAccessTransformerClass() {
